6f13e110c788c83711185a74598f38435b02cc0d
naught <sdax05@gmail.com>
2016-01-27 01:18:02 +0900

    fix post method

--- dist/polling/poll.js
+++ dist/polling/poll.js
@@ -4,31 +4,34 @@ var Consts = require('./../consts');
 var Utils = require("./../utils");
 "use strict";
 var Poll = (function () {
-    function Poll(cookieJar) {
-        this.requestWithJar = request.defaults({ jar: cookieJar });
+    function Poll() {
     }
     Poll.prototype.pollAll = function (skypeAccount, messagesCallback) {
         var _this = this;
-        setTimeout(function () {
-            _this.requestWithJar.post(Consts.SKYPEWEB_HTTPS + skypeAccount.messagesHost + '/v1/users/ME/endpoints/SELF/subscriptions/0/poll', {
-                headers: {
-                    'RegistrationToken': skypeAccount.registrationTokenParams.raw
-                }
-            }, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
+        var headers = {
+            'RegistrationToken': skypeAccount.registrationTokenParams.raw
+        };
+        var options = {
+            url: Consts.SKYPEWEB_HTTPS + skypeAccount.messagesHost + '/v1/users/ME/endpoints/SELF/subscriptions/0/poll',
+            method: 'POST',
+            headers: headers
+        };
+        request(options, function (error, response, body) {
+            if (!error) {
+                if (response.statusCode === 200) {
                     Poll.parsePollResult(JSON.parse(body), messagesCallback);
                 }
                 else {
                     Utils.throwError('Failed to poll messages.');
                 }
-                _this.pollAll(skypeAccount, messagesCallback);
-            });
-        }, 1000);
+            }
+            _this.pollAll(skypeAccount, messagesCallback);
+        });
     };
     Poll.parsePollResult = function (pollResult, messagesCallback) {
         if (pollResult.eventMessages) {
             var messages = pollResult.eventMessages.filter(function (item) {
-                return item.resourceType === 'NewMessage';
+                return item.resourceType === 'NewMessage'; //Fixme there are a lot more EventMessage's types!
             });
             if (messages.length) {
                 messagesCallback(messages);
@@ -38,4 +41,3 @@ var Poll = (function () {
     return Poll;
 })();
 module.exports = Poll;
-//# sourceMappingURL=poll.js.map
--- src/polling/poll.ts
+++ src/polling/poll.ts
@@ -3,32 +3,30 @@ import request = require('request');
 import Consts = require('./../consts');
 import SkypeAccount = require('./../skype_account');
 import Utils = require("./../utils");
-import http = require('http');
-import {CookieJar} from "request";
 "use strict";
 
 class Poll {
-    private requestWithJar;
-
-    constructor(cookieJar:CookieJar) {
-        this.requestWithJar = request.defaults({jar: cookieJar});
-    }
-
     public pollAll(skypeAccount:SkypeAccount, messagesCallback:(messages:Array<any>)=>void) {
-        setTimeout(()=> {
-            this.requestWithJar.post(Consts.SKYPEWEB_HTTPS + skypeAccount.messagesHost + '/v1/users/ME/endpoints/SELF/subscriptions/0/poll', {
-                headers: {
-                    'RegistrationToken': skypeAccount.registrationTokenParams.raw
-                }
-            }, (error:any, response:http.IncomingMessage, body:any) => {
-                if (!error && response.statusCode === 200) {
+        var _this = this;
+        var headers = {
+            'RegistrationToken': skypeAccount.registrationTokenParams.raw
+        };
+        var options = {
+            url: Consts.SKYPEWEB_HTTPS + skypeAccount.messagesHost + '/v1/users/ME/endpoints/SELF/subscriptions/0/poll',
+            method: 'POST',
+            headers: headers
+        };
+        request(options, function(error, response, body) {
+            if (!error) {
+                if (response.statusCode === 200) {
                     Poll.parsePollResult(JSON.parse(body), messagesCallback);
-                } else {
+                }
+                else {
                     Utils.throwError('Failed to poll messages.');
                 }
-                this.pollAll(skypeAccount, messagesCallback);
-            });
-        }, 1000);
+            }
+            _this.pollAll(skypeAccount, messagesCallback);
+        };
     }
 
     private static parsePollResult(pollResult:any, messagesCallback:(messages:Array<any>)=>void) {
